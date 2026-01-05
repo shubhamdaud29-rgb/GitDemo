050f4eef80292cafce6244836153cbe03dbcc43c
shubham <shubhamdaud29@gmail.com>
2026-01-05 21:10:10 +0530

    this is my first GitHub project...!

---
+++ DemoProjectGitHub/src/PackageDemo/Project1.java
@@ -0,0 +1,17 @@
+package PackageDemo;
+
+public class Project1 {
+	 static int i =90;
+	public static void main(String[] args) {
+		
+		int add = addition();
+		System.out.println(add);
+	}
+
+	public static int addition()
+	{
+		i = i/5;
+		return i;
+	}
+
+}
